0efd1f51495688b1e5588835a26c83cb648e056b
MHA <98241106+MHA963@users.noreply.github.com>
2025-02-12 15:30:58 +0100

    update

--- Readme.md
+++ Readme.md
@@ -1,2 +1,7 @@
 # python app for reposting from telegram to X 
-now in python, later with javascript :D 
+now in python, later with javascript :D
+
+## Description
+
+* This application fetches the latest news from specified Telegram channels and reposts it to X (formerly known as Twitter). 
+* It runs a scheduler that checks for new updates every 30 minutes and ensures that the daily and monthly tweet limits are not exceeded.
--- telex-app/main.py
+++ telex-app/main.py
@@ -7,13 +7,13 @@ def run_scheduler():
     # running scheduler every 30 minutes
     while True:
         current_minute = datetime.now().minute
-        if current_minute == 0 or current_minute == 30:  # بداية الساعة ونهايتها
+        if current_minute == 0 or current_minute == 30: 
             if can_tweet():
                 news = fetch_latest_news()
                 if news:
                     post_to_twitter(news["text"], news["media"], news["source"])
-            time.sleep(60)  # انتظر دقيقة لتجنب النشر المكرر
-        time.sleep(10)  # تحقق كل 10 ثوانٍ
-
+            time.sleep(60)  
+        time.sleep(10)  
+        
 if __name__ == "__main__":
     run_scheduler()
